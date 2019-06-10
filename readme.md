2479484092c02b8c2c092aef78656efa93241ff4
Flowuerti <florian@wuertenberger-design.ch>
2019-06-10 17:51:57 +0200

    Init commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+## About WD CMS
+
+## Development Team
+WD CMS was created by Florian Würtenberger (@florianwuerti) continue to develop the platform.
+
+##Foundation library
+The CMS uses Laravel as a foundation PHP framework.
+
+## License
+
+The Laravel framework is open-source software licensed under the [MIT license](https://opensource.org/licenses/MIT).
---
+++ resources/views/layouts/manage.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('page_title', '') - @yield('site_title', 'AOOD Dokumentation')</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+
+@include('_includes.nav.main')
+
+@include('_includes.nav.manage')
+<div class="management-area" id="app">
+
+    <main>
+        @yield('content')
+    </main>
+
+</div>
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
+
+@yield('scripts')
+
+</body>
+</html>
